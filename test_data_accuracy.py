46ef662df07fcfe4e7f0fd18ee8d837d9c7105d0
Byleth <jiang.chengjun@icloud.com>
2025-03-28 20:16:44 +0800

    年份逻辑更新

--- test_data_accuracy.py
+++ test_data_accuracy.py
@@ -17,6 +17,11 @@ school_name = os.getenv("SCHOOL_NAME")
 value = os.getenv("VALUE")
 year = os.getenv("YEAR")
 
+# indicator = "党建工作示范单位"
+# school_name = "山东外国语职业技术大学"
+# value = "1"
+# year = "2023"
+
 # 打印参数（可选，用于调试）
 # print(f"Indicator: {indicator}, School Name: {school_name}, Value: {value}, Year: {year}")
 
@@ -116,28 +121,102 @@ def extract_data(cell):
     return None
 
 
+
+"""
+年份有三种情况
+1. 截止xxxx前, 不可点击, 需判断测试年份是否在截止日期前
+2. 时间段, 需选择开始及结束时间, 需判断是否可以选择核验年份
+3. 时间点, 直接选择年份, 需判断是否可以选择核验年份
+"""
 # 全部指标页面查看监测年份
-def check_year_in_all_ind(driver):
+def check_year(driver):
     global year
-    # 打开监测年份窗口
-    window = f"//input[@placeholder='选择年份']"
-    driver.find_element(By.XPATH, window).click()
-    # 查找当前选择监测年份
-    current_year_path = f"//div//ul[@class='picker-date-list']/li[@class='active-item']/span"
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, current_year_path)))
-    current_year = driver.find_element(By.XPATH, current_year_path).text.strip()
-    # print("当前年份："+current_year)
-    # 年份调整
-    # print("当前年份:"+current_year+";目标年份:"+year)
-    if year != current_year:
-        current_year = alter_year_in_all_ind(driver)
-    driver.find_element(By.XPATH, window).click()
-    # print("返回年份："+current_year)
+    # 判断是否为年份情况1
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.any_of(
+                EC.presence_of_element_located((By.XPATH, "//input[@placeholder='选择年份']")),
+                EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '截至')]"))
+            )
+        )
+        # 找到输入框, 年份情况2,3, 分类讨论
+        if "input" in element.tag_name:
+            # 打开监测年份窗口
+            window = f"//input[@placeholder='选择年份']"
+            driver.find_element(By.XPATH, window).click()
+            # 判断年份情况
+            current_year = None
+            for li in driver.find_elements(By.XPATH, "//ul[@class='picker-date-list']/li"):
+                if "active-item" in li.get_attribute("class"):
+                    current_year = li.find_element(By.CLASS_NAME, "inner").text  # 提取年份
+                    break
+            # 年份情况3
+            if current_year:
+                print("当前年份：" + current_year)
+                if year != current_year:
+                    current_year = alter_year_point_section(driver)
+                driver.find_element(By.XPATH, window).click()
+                # print("返回年份："+current_year)
+                return current_year
+            # 年份情况2
+            else:
+                current_year = alter_year_range_section(driver)
+                return current_year
+        # 找到年份情况1
+        else:
+            current_year = driver.find_element(By.XPATH, "//span[contains(text(), '截至')]").text
+            # print("页面年份显示为：" + current_year)
+            current_year = check_year_before_deadline(driver, current_year)
+            return current_year
+    except:
+        assert f"页面年份相关元素加载失败"
+
+# 截止日期前日期判断
+# year全局变量改写
+def check_year_before_deadline(driver, current_year):
+    global year
+    deadline = int(current_year[2:6])
+    assert int(year) <= deadline, f"目标年份{year}不在截至日期内, 请查看数据是否尚未更新或是否出现其他异常!"
+    year = current_year
     return current_year
 
+# 时间段年份调整
+def alter_year_range_section(driver):
+    global year
+    # 判断核验年份是否在年份段中
+    title = f"//div[@class='picker-date-header']/span[@class='text']"
+    str_year = driver.find_element(By.XPATH, title).text
+    print(str_year)
+    start, end = str_year.split('-')
+    while int(year) > int(end):
+        str_year = driver.find_element(By.XPATH, title).text
+        print("当前年份范围:", str_year)
+        start, end = str_year.split('-')
 
-# 全部指标页面年份调整
-def alter_year_in_all_ind(driver):
+        # 判断年份是否在区间内
+        if (int(year) <= int(end)) and (int(year) >= int(start)):
+            print(f"已到达目标年份范围（{year} ≤ {end}），停止翻页")
+            break
+
+        # 不在区间内, 进行翻页操作
+        if int(year) <= int(start):
+            driver.find_element(By.XPATH, "//div[@class='picker-date-header']/span[1]").click()
+        else:
+            driver.find_element(By.XPATH, "//div[@class='picker-date-header']/span[3]").click()
+
+    # 进行年份选择
+    target_year_path = f"//div//ul[@class='picker-date-list']//span[contains(text(), '{year}')]"
+    year_element = driver.find_element(By.XPATH, target_year_path)
+    class_value = year_element.get_attribute("class")
+    assert "disable-item" not in class_value, f"目标年份{year}不可选,数据尚未更新或出现其他异常!"
+    # 开始与结束选择同一年份
+    driver.find_element(By.XPATH, target_year_path).click()
+    driver.find_element(By.XPATH, target_year_path).click()
+    return year
+
+
+# 时间点年份调整
+def alter_year_point_section(driver):
     global year
     window = f"//input[@placeholder='选择年份']"
     target_year_path = f"//div//ul[@class='picker-date-list']/li/span[contains(text(), '{year}')]"
@@ -153,8 +232,13 @@ def alter_year_in_all_ind(driver):
 def percent_to_float(percent_str):
     return double(percent_str[:-1]) / 100
 
-def is_equal(float1, float2, tolerance=1e-7):
-    return abs(double(float2) - double(float1) < tolerance)
+def is_equal(float1, float2, tolerance=1e-3):
+    # 四舍五入到小数点后3位
+    rounded_float1 = round(double(float1), 3)
+    rounded_float2 = round(double(float2), 3)
+
+    # 比较差值是否在允许的误差范围内（默认 0.001）
+    return abs(rounded_float2 - rounded_float1) <= tolerance
 
 def test_jump_to_target_school(driver):
     global school_name
@@ -253,12 +337,19 @@ def test_data_overall(driver):
         data_dict_list.append(row_dict)
         print(row_dict)
 
-    assert year == data_dict_list[0]["监测年份"], (
-        f"{Fore.BLUE}=" * 80 + Style.RESET_ALL + "\n"
-        f"{Fore.RED}总体定位页面核验结果" + Style.RESET_ALL + "\n"
-        f"{Fore.RED}状态: {Fore.RED}✖ 年份有误！{Style.RESET_ALL}\n"
-        # f"{Fore.BLUE}=" * 80 + Style.RESET_ALL + "\n"
-    )
+    # 年份处理
+    if "截至" in data_dict_list[0]["监测年份"]:
+        check_year_before_deadline(driver, data_dict_list[0]["监测年份"])
+    # 监测年份与数据年份不一致
+    else:
+        assert year == data_dict_list[0]["监测年份"], (
+            f"{Fore.BLUE}=" * 80 + Style.RESET_ALL + "\n"
+            f"{Fore.RED}总体定位页面核验结果" + Style.RESET_ALL + "\n"
+            f"{Fore.RED}状态: {Fore.RED}✖ 数据更新年份为{year},页面年份为{data_dict_list[0]["监测年份"]},无法进行核验！{Style.RESET_ALL}\n"
+            # f"{Fore.BLUE}=" * 80 + Style.RESET_ALL + "\n"
+        )
+
+    # 数据核验
     assert is_equal(value, data_dict_list[0]["指标数据"]), (
         f"{Fore.BLUE}=" * 80 + Style.RESET_ALL + "\n"
         f"{Fore.RED}总体定位页面核验结果" + Style.RESET_ALL + "\n"
@@ -321,7 +412,7 @@ def test_data_all_ind(driver):
     driver.find_element(By.XPATH, verify).click()
 
     # 年份选择,若年份非数据更新年份,则进行调整
-    current_year = check_year_in_all_ind(driver)
+    current_year = check_year(driver)
 
     # 提取数据
     sleep(1)
@@ -330,12 +421,19 @@ def test_data_all_ind(driver):
     headers = [header.text.strip() for header in driver.find_elements(By.XPATH,
                                                                       "//th[contains(@class, 'vxe-header--column')]//span[@class='custom-name'] | //th[contains(@class, 'vxe-header--column')]//div[@class='none-border']")]
     # print(headers)
-    values = [value.text.strip() for value in driver.find_elements(By.XPATH,
-                                                                   "//td[contains(@class, 'vxe-body--column')]//*[self::div or self::span][(not(*) or @title='未公布') and not(contains(., '重置'))]")]
-    for i in range(len(values)):
-        if values[i] == "":
-            values[i] = "0.00"
-    # print(values)
+    # 直接获取所有 `<td>` 的文本，不跳过空值
+    values = []
+    td_elements = driver.find_elements(By.XPATH, "//td[contains(@class, 'vxe-body--column')]")
+    for td in td_elements:
+        # 尝试获取内部 div/span 的文本，如果没有则取 td 本身的文本
+        inner_text = td.find_elements(By.XPATH,
+                                      ".//*[self::div or self::span][(not(*) or @title='未公布' or @title='无法计算') and not(contains(., '重置'))]")
+        if inner_text:  # 如果有子元素
+            text = inner_text[0].text.strip()
+        else:  # 如果没有子元素，直接取 td 的文本
+            text = td.text.strip()
+        values.append(text if text else "0")  # 空值替换为 "0.00"
+    print(values)
 
     # 提取当前监测年份数据
     if len(headers) > len(values):
@@ -409,14 +507,25 @@ def test_data_ind_check(driver):
     values = [value.text.strip() for value in driver.find_elements(By.XPATH,
                                                                    "//td[contains(@class, 'vxe-body--column')]//*[self::div or self::span][not(*) and normalize-space()]")]
     # values = driver.find_elements(By.XPATH,"//td[contains(@class, 'vxe-body--column')]//*[self::div or self::span][not(*) and normalize-space() and not(contains(., '重置'))]")
-    # print(values)
-    if len(values) < len(headers):
-        values += ["0"] * (len(headers) - len(values))
+    # 直接获取所有 `<td>` 的文本，不跳过空值
+    values = []
+    td_elements = driver.find_elements(By.XPATH, "//td[contains(@class, 'vxe-body--column')]")
+    for td in td_elements:
+        # 尝试获取内部 div/span 的文本，如果没有则取 td 本身的文本
+        inner_text = td.find_elements(By.XPATH,
+                                      ".//*[self::div or self::span][(not(*) or @title='未公布' or @title='无法计算') and not(contains(., '重置'))]")
+        if inner_text:  # 如果有子元素
+            text = inner_text[0].text.strip()
+        else:  # 如果没有子元素，直接取 td 的文本
+            text = td.text.strip()
+        values.append(text if text else "0")  # 空值替换为 "0.00"
+    values = values[1:]
+    print(values)
     # print(headers)
-    # print(values)
     # 存为字典
     data_dict = dict(zip(headers, values))
     print(data_dict)
+
     value_page = data_dict[year]
     if data_dict[year].endswith("%"):
         data_dict[year] = percent_to_float(data_dict[year])
@@ -475,13 +584,13 @@ def test_data_feedback(driver):
 
     # 选择指标
     ind_select = f"//label[@class='ant-checkbox-wrapper']/span[input[@type='checkbox']]"
-    WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, ind_select)))
+    # WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, ind_select)))
     driver.find_element(By.XPATH, ind_select).click()
     verify = f"//button[span[contains(text(), '确认')]]"
     driver.find_element(By.XPATH, verify).click()
 
     # 年份选择,若年份非数据更新年份,则进行调整
-    current_year = check_year_in_all_ind(driver)
+    current_year = check_year(driver)
 
     # 提取数据
     sleep(1)
@@ -492,9 +601,8 @@ def test_data_feedback(driver):
     # print(headers)
     values = [value.text.strip() for value in driver.find_elements(By.XPATH,
                                                                    "//td[contains(@class, 'vxe-body--column')]//*[self::div or self::span][(not(*) or @title='未公布') and not(contains(., '重置'))]")]
-    for i in range(len(values)):
-        if values[i] == "":
-            values[i] = "0"
+    # 加入年份数据
+    values.insert(4, year)
     # print(values)
 
     # 提取当前监测年份数据
@@ -508,16 +616,16 @@ def test_data_feedback(driver):
     data_dict = dict(zip(headers, values))
     # 判断前端的文字为“数据反馈”（0）还是“数据填报”（1）
     flag = 0
-    value_page = data_dict["监测年份"]
-    if data_dict["监测年份"].endswith("%"):
-        data_dict["监测年份"] = percent_to_float(data_dict["监测年份"])
-    if data_dict["监测年份"] == "未采集到，请填报":
-        data_dict["监测年份"] = 0
+    value_page = data_dict["数据"]
+    if data_dict["数据"].endswith("%"):
+        data_dict["数据"] = percent_to_float(data_dict["数据"])
+    if data_dict["数据"] == "未采集到，请填报":
+        data_dict["数据"] = 0
         flag = 1
-    # print(data_dict)
+    print(data_dict)
 
     # 总页面数据核验
-    assert is_equal(value, data_dict["监测年份"]),  (
+    assert is_equal(value, data_dict["数据"]),  (
         f"{Fore.BLUE}=" * 80 + Style.RESET_ALL + "\n"
         f"{Fore.RED}数据反馈页面" + Style.RESET_ALL + "\n"
         f"{Fore.GREEN}指标: {Fore.YELLOW}{indicator}{Style.RESET_ALL}\n"
@@ -540,6 +648,12 @@ def test_data_feedback(driver):
     )
     # 分隔线
     print(Fore.BLUE + "=" * 80 + Style.RESET_ALL)
+
+    '''
+        待完善数据反馈、明细反馈逻辑
+    '''
+    if "截至" in year:
+        assert 1 < 0, f"明细反馈待完善"
 
     # 数据反馈操作页面数据核验
     if flag == 0:
